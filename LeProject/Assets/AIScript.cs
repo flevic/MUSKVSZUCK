1a6fdce7b337b44198144e89944d922c70d0c50f
flevic <filip.levic@email.cz>
2023-10-10 10:26:56 +0200

    AIScrapped
    coop works

--- LeProject/Assets/AIScript.cs
+++ LeProject/Assets/AIScript.cs
@@ -4,7 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerScript : MonoBehaviour
+public class AIScript : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float jumpForce = 10.0f;
@@ -30,8 +30,8 @@ public class PlayerScript : MonoBehaviour
 
     private void Update()
     {
-        
-        float horizontal = Input.GetAxis("Horizontal");
+
+        float horizontal = Input.GetAxis("Horizontal2");
 
         Vector2 movement = new Vector2(horizontal, 0);
 
@@ -40,7 +40,7 @@ public class PlayerScript : MonoBehaviour
         // Check if the player is grounded using OverlapCircle
         isGrounded = Physics2D.OverlapCircle(transform.position, 2.5f, groundLayer);
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump2"))
         {
             // Apply an upward force for jumping
             rigidbody2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -50,35 +50,35 @@ public class PlayerScript : MonoBehaviour
         {
             // Check if the AI character is within the trigger zone.
             Debug.Log("KYS");
-                
-                    // Trigger the punch animation or attack here if you have one.
-                    
-                        AIController aiHealth = GetComponent<AIController>();
-                        if (aiHealth != null)
-                        {
-                            aiHealth.TakeDamage(punchDamage);
-                        }
 
-                        // Set the time for the next punch.
-                        nextPunchTime = Time.time + punchDelay;
-                    
-                    // Apply damage to the AI character.
+            // Trigger the punch animation or attack here if you have one.
 
-                
+            AIController aiHealth = GetComponent<AIController>();
+            if (aiHealth != null)
+            {
+                aiHealth.TakeDamage(punchDamage);
             }
-        
 
-        
+            // Set the time for the next punch.
+            nextPunchTime = Time.time + punchDelay;
+
+            // Apply damage to the AI character.
+
+
+        }
+
+
+
 
         if (horizontal > 0)
         {
             // Player is moving to the right
-            transform.rotation = Quaternion.Euler(0, 0, 0); // No rotation
+            transform.rotation = Quaternion.Euler(0, 180, 0); // No rotation
         }
         else if (horizontal < 0)
         {
             // Player is moving to the left
-            transform.rotation = Quaternion.Euler(0, 180, 0); // Flip on the Y-axis
+            transform.rotation = Quaternion.Euler(0, 0, 0); // Flip on the Y-axis
         }
     }
 
